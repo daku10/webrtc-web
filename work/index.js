cd04ba7998c4f61d9908d811c17bff4bc18aa077
daku10 <daku.two@gmail.com>
2021-10-02 20:22:08 +0900

    finish step06

--- work/index.js
+++ work/index.js
@@ -14,7 +14,7 @@ var app = http
 
 var io = socketIO.listen(app);
 io.sockets.on("connection", function (socket) {
-  // convenience function to log server mesage on the client
+  // convenience function to log server messages on the clinet
   function log() {
     var array = ["Message from server:"];
     array.push.apply(array, arguments);
@@ -22,33 +22,33 @@ io.sockets.on("connection", function (socket) {
   }
 
   socket.on("message", function (message) {
-    log("Client said:", message);
+    log("Client said: ", message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit("message", message);
   });
 
   socket.on("create or join", function (room) {
-    log("Received request to create or join room " + room);
+    log(`Received request to create to join room ${room}`);
 
     var clientsInRoom = io.sockets.adapter.rooms[room];
     var numClients = clientsInRoom
       ? Object.keys(clientsInRoom.sockets).length
       : 0;
-
-    log("Room " + room + " now has " + numClients + " client(s)");
+    log(`Room ${room} now has ${numClients} client(s)`);
 
     if (numClients === 0) {
       socket.join(room);
-      log("Client ID " + socket.id + " created room " + room);
+      log(`Client ID ${socket.id} created room ${room}`);
       socket.emit("created", room, socket.id);
     } else if (numClients === 1) {
-      log("Client ID " + socket.id + " joined room " + room);
-      io.sockets.in(room).emit("join", room);
+      log(`Client ID ${socket.id} joined room ${room}`);
+      // io.sockets.in(room).emit('join', room);
+      console.log("joined!!");
       socket.join(room);
       socket.emit("joined", room, socket.id);
-      io.sockets.in(room).emit("ready");
+      io.sockets.in(room).emit("ready", room);
+      socket.broadcast.emit("ready", room);
     } else {
-      // max two clients
       socket.emit("full", room);
     }
   });
@@ -64,7 +64,12 @@ io.sockets.on("connection", function (socket) {
     }
   });
 
-  socket.on("bye", function () {
-    console.log("received bye");
+  socket.on("disconnect", function (reason) {
+    console.log(`Peer or server disconnected. Reason: ${reason}.`);
+    socket.broadcast.emit("bye");
+  });
+
+  socket.on("bye", function (room) {
+    console.log(`Peer said bye on room ${room}.`);
   });
 });
